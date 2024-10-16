d1cb64ce81e2341fc4f9d8a10e9bbbcb2d517333
ananas-block <jorrit@lightprotocol.com>
2024-10-16 04:05:24 +0100

    fix: add light prover client batch update and append tests
    added bloomfilter
    
    batch e2e test failing because filter change doesn't work yet
    
    debugging
    
    batched Merkle tree e2e test works with output queue
    
    fix: e2e test work with input and output queues
    
    chore: rename wipe bloomfilter -> wipe_batch
    
    refactored input queue into mt account
    
    stash
    
    test: rnd account init works
    
    chore: replace is_inserted with state
    
    chore: moved offset to bounded vec, added bounded vec init and deserialize
    
    fixed bounded vec imports
    
    refactor: partial batch zkp updates, flexible sized root history array with seq
    
    implemented batch prove and verification in rust tests untested
    
    stash: batched updates work with simulate transactions test
    
    add comment
    
    stash before account *mut
    
    onchain account init works
    
    test: account compression program batch updates work
    
    feat: add flexbile nullifier to circuit
    
    feat: implement nullifier derivation from tx hash
    
    test: batch
    
    test: bounded vec
    
    cleanup
    
    cleanup
    
    cleanup
    
    feat: add flexible nullifier to update circuit
    
    fix: e2e test
    
    chore: add full asserts to simulate tx test
    
    test: root overwrite test
    
    cleanup

---
+++ circuit-lib/light-prover-client/src/mock_batched_forester.rs
@@ -0,0 +1,181 @@
+use std::fmt::Error;
+
+use light_hasher::{Hasher, Poseidon};
+use light_merkle_tree_reference::MerkleTree;
+use light_utils::bigint::bigint_to_be_bytes_array;
+use reqwest::Client;
+
+use crate::{
+    batch_append::{calculate_hash_chain, get_batch_append_inputs},
+    batch_update::get_batch_update_inputs,
+    gnark::{
+        batch_append_json_formatter::append_inputs_string,
+        batch_update_json_formatter::update_inputs_string,
+        constants::{PROVE_PATH, SERVER_ADDRESS},
+        proof_helpers::{compress_proof, deserialize_gnark_proof_json, proof_from_json_struct},
+    },
+};
+
+// TODO: rename to MockBatchedForester
+pub struct MockBatchedForester<const HEIGHT: usize> {
+    pub merkle_tree: MerkleTree<Poseidon>,
+    pub input_queue_leaves: Vec<[u8; 32]>,
+    /// Indices of leaves which in merkle tree which are active.
+    pub output_queue_leaves: Vec<[u8; 32]>,
+    pub active_leaves: Vec<[u8; 32]>,
+    pub tx_events: Vec<MockTxEvent>,
+}
+
+#[derive(Debug, Clone)]
+pub struct MockTxEvent {
+    pub tx_hash: [u8; 32],
+    pub inputs: Vec<[u8; 32]>,
+    pub outputs: Vec<[u8; 32]>,
+}
+
+impl<const HEIGHT: usize> Default for MockBatchedForester<HEIGHT> {
+    fn default() -> Self {
+        let merkle_tree = MerkleTree::<Poseidon>::new(HEIGHT, 0);
+        let input_queue_leaves = vec![];
+        Self {
+            merkle_tree,
+            input_queue_leaves,
+            output_queue_leaves: vec![],
+            active_leaves: vec![],
+            tx_events: vec![],
+        }
+    }
+}
+
+impl<const HEIGHT: usize> MockBatchedForester<HEIGHT> {
+    pub async fn get_batched_append_proof(
+        &mut self,
+        next_index: usize,
+        leaves: Vec<[u8; 32]>,
+        num_zkp_updates: u32,
+        batch_size: u32,
+    ) -> Result<(CompressedProof, [u8; 32], [u8; 32]), Error> {
+        let start = num_zkp_updates as usize * batch_size as usize;
+        let end = start + batch_size as usize;
+        let leaves = leaves[start..end].to_vec();
+        let sub_trees = self.merkle_tree.get_subtrees().try_into().unwrap();
+        let local_leaves_hashchain = calculate_hash_chain(&leaves);
+        for leaf in leaves.iter() {
+            self.merkle_tree.append(leaf).unwrap();
+        }
+        let circuit_inputs = get_batch_append_inputs::<HEIGHT>(
+            next_index,
+            sub_trees,
+            leaves,
+            local_leaves_hashchain,
+        );
+        let client = Client::new();
+        let inputs = append_inputs_string(&circuit_inputs);
+
+        let response_result = client
+            .post(&format!("{}{}", SERVER_ADDRESS, PROVE_PATH))
+            .header("Content-Type", "text/plain; charset=utf-8")
+            .body(inputs)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        if response_result.status().is_success() {
+            let body = response_result.text().await.unwrap();
+            let proof_json = deserialize_gnark_proof_json(&body).unwrap();
+            let (proof_a, proof_b, proof_c) = proof_from_json_struct(proof_json);
+            let (proof_a, proof_b, proof_c) = compress_proof(&proof_a, &proof_b, &proof_c);
+            return Ok((
+                CompressedProof {
+                    a: proof_a,
+                    b: proof_b,
+                    c: proof_c,
+                },
+                bigint_to_be_bytes_array::<32>(&circuit_inputs.new_root.to_biguint().unwrap())
+                    .unwrap(),
+                bigint_to_be_bytes_array::<32>(
+                    &circuit_inputs.new_sub_tree_hash_chain.to_biguint().unwrap(),
+                )
+                .unwrap(),
+            ));
+        }
+        Err(Error)
+    }
+
+    pub async fn get_batched_update_proof(
+        &mut self,
+        batch_size: u32,
+        leaves_hashchain: [u8; 32],
+    ) -> Result<(CompressedProof, [u8; 32]), Error> {
+        let mut merkle_proofs = vec![];
+        let mut path_indices = vec![];
+        let leaves = self.input_queue_leaves[..batch_size as usize].to_vec();
+        let old_root = self.merkle_tree.root();
+        let mut nullifiers = Vec::new();
+        let mut tx_hashes = Vec::new();
+        for leaf in leaves.iter() {
+            let index = self.merkle_tree.get_leaf_index(leaf).unwrap();
+            let proof = self.merkle_tree.get_proof_of_leaf(index, true).unwrap();
+            merkle_proofs.push(proof.to_vec());
+            path_indices.push(index as u32);
+            self.input_queue_leaves.remove(0);
+            let event = self
+                .tx_events
+                .iter()
+                .find(|tx_event| tx_event.inputs.contains(leaf))
+                .expect("No event for leaf found.");
+            let nullifier = Poseidon::hashv(&[leaf, &event.tx_hash]).unwrap();
+            tx_hashes.push(event.tx_hash);
+            nullifiers.push(nullifier);
+            self.merkle_tree.update(&nullifier, index).unwrap();
+        }
+        // local_leaves_hashchain is only used for a test assertion.
+        let local_leaves_hashchain = calculate_hash_chain(&nullifiers);
+        assert_eq!(leaves_hashchain, local_leaves_hashchain);
+        // TODO: adapt update circuit to allow for non-zero updates
+        let inputs = get_batch_update_inputs::<HEIGHT>(
+            old_root,
+            tx_hashes,
+            leaves,
+            leaves_hashchain,
+            merkle_proofs,
+            path_indices,
+            batch_size,
+        );
+        let client = Client::new();
+        let circuit_inputs_new_root =
+            bigint_to_be_bytes_array::<32>(&inputs.new_root.to_biguint().unwrap()).unwrap();
+        let inputs = update_inputs_string(&inputs);
+        let new_root = self.merkle_tree.root();
+        assert_eq!(circuit_inputs_new_root, new_root);
+
+        let response_result = client
+            .post(&format!("{}{}", SERVER_ADDRESS, PROVE_PATH))
+            .header("Content-Type", "text/plain; charset=utf-8")
+            .body(inputs)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        if response_result.status().is_success() {
+            let body = response_result.text().await.unwrap();
+            let proof_json = deserialize_gnark_proof_json(&body).unwrap();
+            let (proof_a, proof_b, proof_c) = proof_from_json_struct(proof_json);
+            let (proof_a, proof_b, proof_c) = compress_proof(&proof_a, &proof_b, &proof_c);
+            return Ok((
+                CompressedProof {
+                    a: proof_a,
+                    b: proof_b,
+                    c: proof_c,
+                },
+                new_root,
+            ));
+        }
+        Err(Error)
+    }
+}
+
+pub struct CompressedProof {
+    pub a: [u8; 32],
+    pub b: [u8; 64],
+    pub c: [u8; 32],
+}
